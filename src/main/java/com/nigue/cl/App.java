53803ed158e1a0729dd9cfc9dc1d7d9ad9cb3490
nigue <anibaldavor@gmail.com>
2016-10-30 17:58:59 -0300

    avances

--- src/main/java/com/nigue/cl/App.java
+++ src/main/java/com/nigue/cl/App.java
@@ -1,5 +1,6 @@
 package com.nigue.cl;
 
+import com.nigue.cl.file.RecursiveFileDisplay;
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
@@ -13,38 +14,41 @@ public class App {
         String fileToFiles = args[0];
         String newFolder = args[1];
 
-//        String fileToFiles = "/home/anibal/Comparacion/pdfCartolaFFMM/puro/mioc/insourcingPdf/data_mioc_pdfCartolaFFMM.txt";
-//        String newFolder = "/home/anibal/Comparacion/pdfCartolaFFMM/desc/mio2/";
-
+//        String fileToFiles = "C:\\Users\\ibanio\\Documents\\4_DigitosPdf\\puro\\mioc\\EnvioCartolas.java";
+//        String newFolder = "C:\\Users\\ibanio\\Documents\\4_DigitosPdf\\desc\\mioc";
         List<String> files = new ArrayList<String>();
 
         File file = new File(fileToFiles);
         if (file.isFile()) {
-            try {
-                BufferedReader br = new BufferedReader(new FileReader(file));
-                String sCurrentLine;
-                while ((sCurrentLine = br.readLine()) != null) {
-                    files.add(sCurrentLine);
+            if (file.getAbsolutePath().contains(".class")) {
+                files.add(file.getAbsolutePath());
+            } else {
+                try {
+                    BufferedReader br = new BufferedReader(new FileReader(file));
+                    String sCurrentLine;
+                    while ((sCurrentLine = br.readLine()) != null) {
+                        files.add(sCurrentLine);
+                    }
+                } catch (IOException e) {
+                    e.printStackTrace();
                 }
-            } catch (IOException e) {
-                e.printStackTrace();
             }
         } else {
             System.out.println(fileToFiles + " It's not a file");
+            RecursiveFileDisplay rfd = new RecursiveFileDisplay();
+            files = rfd.displayDirectoryContents(file);
         }
 
         File folder = new File(newFolder);
         if (folder.isDirectory()) {
             ProyconProcess p = new ProyconProcess(files, folder);
             p.execute();
-        } else {
-            if (!folder.exists()) {
-                if (folder.mkdirs()) {
-                    ProyconProcess p = new ProyconProcess(files, folder);
-                    p.execute();
-                } else {
-                    System.out.println("Failed to create multiple directories!");
-                }
+        } else if (!folder.exists()) {
+            if (folder.mkdirs()) {
+                ProyconProcess p = new ProyconProcess(files, folder);
+                p.execute();
+            } else {
+                System.out.println("Failed to create multiple directories!");
             }
         }
     }
